5e984c50cfde769daf4f4a2ee92b924aaf564c9c
Stephanie Chan <stephanite.chan@gmail.com>
2025-10-01 23:08:11 +0800

    Can't get clear button to work

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,7 +1,33 @@
-const addItem = () => {
+let shoppingListItems = [];
 
+// let listElement = document.getElementById("shopping-list-items");
+
+// for (const shoppingItem of shoppingListItems){
+//     console.log(shoppingItem);
+//     let itemElement = document.createElement("li");
+//     itemElement.innerText = shoppingItem;
+//     listElement.appendChild(itemElement);
+// }
+
+const addItem = () => {
+    let item = document.getElementById("new-item-text").value;
+    shoppingListItems = [...shoppingListItems, item];
+    updateItems();
+    document.getElementById("new-item-text").value = "";
 };
 
 const updateItems = () => {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
 
+    for (const shoppingItem of shoppingListItems){
+        let itemElement = document.createElement("li");
+        itemElement.innerText = shoppingItem;
+        listElement.appendChild(itemElement);
+    }
 };
+
+let clearItems = () => {
+    document.getElementById("shopping-list-items").replaceChildren();
+};
+
